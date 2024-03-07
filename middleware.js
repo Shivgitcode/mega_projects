ccd16a7da035f86af8894896b586dd0355295bb6
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-03-07 22:11:22 +0530

    added current user functionality

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const isLoggedIn = (req, res, next) => {
+    // console.log("REQ.User...", req.user);
     if (!req.isAuthenticated()) {
         req.flash('error', 'you must be signed in first!');
         return res.redirect('/login')
